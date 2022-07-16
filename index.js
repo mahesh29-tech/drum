f627c54a9f4d8723c39e0c6dd96d905aaa068aed
mahesh29-tech <85659355+mahesh29-tech@users.noreply.github.com>
2022-07-16 19:01:27 +0530

    all sounds removed

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function makeSound(key) {
       break;
 
     case "j":
-      var snare = new Audio("sounds/snare.mp3");
+      var snare = new Audio("snare.mp3");
       snare.play();
       break;
 
