3d7cb0283dfb3b2bed3f1b37ee5701ce9c3d8e2f
mahesh29-tech <85659355+mahesh29-tech@users.noreply.github.com>
2022-07-16 22:07:13 +0530

    qwertyu changed

--- index.js
+++ index.js
@@ -26,37 +26,37 @@ document.addEventListener("keypress", function(event) {
 function makeSound(key) {
 
   switch (key) {
-    case "w":
+    case "q":
       var tom1 = new Audio("tom-1.mp3");
       tom1.play();
       break;
 
-    case "a":
+    case "w":
       var tom2 = new Audio("tom-2.mp3");
       tom2.play();
       break;
 
-    case "s":
+    case "e":
       var tom3 = new Audio("tom-3.mp3");
       tom3.play();
       break;
 
-    case "d":
+    case "r":
       var tom4 = new Audio("tom-4.mp3");
       tom4.play();
       break;
 
-    case "j":
+    case "t":
       var snare = new Audio("snare.mp3");
       snare.play();
       break;
 
-    case "k":
+    case "y":
       var crash = new Audio("crash.mp3");
       crash.play();
       break;
 
-    case "l":
+    case "u":
       var kick = new Audio("kick-bass.mp3");
       kick.play();
       break;
